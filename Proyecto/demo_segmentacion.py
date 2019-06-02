f14f24ab4e3e8c76257e33a5e030b0a6fb2445b4
User <lmunar10@paarbelaez003.pym3om3cqq0uxjwr13sm4retbd.bx.internal.cloudapp.net>
2019-06-02 23:49:18 +0000

    correccion demos segmentacion

--- Proyecto/demo_segmentacion.py
+++ Proyecto/demo_segmentacion.py
@@ -14,6 +14,50 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 
+import ipdb 
+import requests    
+import tarfile
+import os
+import zipfile
+
+if not os.path.isdir(os.path.join(os.getcwd(),'ISIC2018_Task1-2_Training_Input')):
+   print('It will be proceed to download the training database')
+   url='https://challenge.kitware.com/api/v1/item/5ac37a9d56357d4ff856e176/download'
+   r=requests.get(url,allow_redirects=True)
+   open('ISIC2018_Task1-2_Training_Input.zip','wb').write(r.content) 
+   print('The training database had been download')
+   print('It will be proceed to decompress de training database')
+   zip_ref = zipfile.ZipFile('ISIC2018_Task1-2_Training_Input.zip', 'r')
+   zip_ref.extractall()
+   zip_ref.close()
+   
+   
+   
+
+url ='https://challenge.kitware.com/api/v1/item/5ac3695656357d4ff856e16a/download'
+
+
+if not os.path.isdir(os.path.join(os.getcwd(),'ISIC2018_Task1_Training_GroundTruth')):
+   print('It will be proceed to download the training database  groundtruht')
+   url='https://challenge.kitware.com/api/v1/item/5ac3695656357d4ff856e16a/download'
+   r=requests.get(url,allow_redirects=True)
+   open('ISIC2018_Task1_Training_GroundTruth.zip','wb').write(r.content) 
+   print('The training database had been download')
+   print('It will be proceed to decompress de training database')
+   zip_ref = zipfile.ZipFile('ISIC2018_Task1_Training_GroundTruth.zip', 'r')
+   zip_ref.extractall()
+   zip_ref.close()
+
+
+
+
+
+
+
+
+
+
+
 import numpy as np
 def imshow(img, seg, ground_truth, title='Image'):
     import matplotlib.pyplot as plt
--- Proyecto/evaluacion_segmentacion.py
+++ Proyecto/evaluacion_segmentacion.py
@@ -16,6 +16,55 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
+import ipdb 
+import requests    
+import tarfile
+import os
+import zipfile
+
+if not os.path.isdir(os.path.join(os.getcwd(),'ISIC2018_Task1-2_Training_Input')):
+   print('It will be proceed to download the training database')
+   url='https://challenge.kitware.com/api/v1/item/5ac37a9d56357d4ff856e176/download'
+   r=requests.get(url,allow_redirects=True)
+   open('ISIC2018_Task1-2_Training_Input.zip','wb').write(r.content) 
+   print('The training database had been download')
+   print('It will be proceed to decompress de training database')
+   zip_ref = zipfile.ZipFile('ISIC2018_Task1-2_Training_Input.zip', 'r')
+   zip_ref.extractall()
+   zip_ref.close()
+   
+   
+   
+
+url ='https://challenge.kitware.com/api/v1/item/5ac3695656357d4ff856e16a/download'
+
+
+if not os.path.isdir(os.path.join(os.getcwd(),'ISIC2018_Task1_Training_GroundTruth')):
+   print('It will be proceed to download the training database  groundtruht')
+   url='https://challenge.kitware.com/api/v1/item/5ac3695656357d4ff856e16a/download'
+   r=requests.get(url,allow_redirects=True)
+   open('ISIC2018_Task1_Training_GroundTruth.zip','wb').write(r.content) 
+   print('The training database had been download')
+   print('It will be proceed to decompress de training database')
+   zip_ref = zipfile.ZipFile('ISIC2018_Task1_Training_GroundTruth.zip', 'r')
+   zip_ref.extractall()
+   zip_ref.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def metricJaccard(groundtruth,segmentation):
     ac=np.zeros(groundtruth.shape)
     ac[segmentation==1]=1       
